448ebd95b9560ac3f2199ccdc241143a675037c6
erainterfacesindia <erainterfacesindia>
2023-01-20 01:22:52 +0530

    materialui update

--- src/Pages/Lists/list.jsx
+++ src/Pages/Lists/list.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./list.scss";
 
 function List() {
-  return <div className="userlist-wrapper">userlist</div>;
+  return <div className="userlist-wrapper">ListData</div>;
 }
 
 export default List;
